cdc27821180ce31d1793728939c6b4c2f49cdf1f
ancyentMariner <jeffreyckingswood@gmail.com>
2017-02-19 17:03:57 -0500

    it works

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
 const Twit = require('twit');
 const express = require('express');
-const port = (process.env.PORT || 3000);
+const app = express();
+var Client = require('node-rest-client').Client;
+var client = new Client();
 
+const port = (process.env.PORT || 3000);
 const secret = {
   consumer_key: process.env.CUSTOMER_KEY,
   consumer_secret: process.env.CUSTOMER_SECRET,
   access_token: process.env.ACCESS_TOKEN_KEY,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
 };
-
 const T = new Twit(secret);
-const app = express();
 
 app.get('/', (req, res) => {
   res.send('hi there');
@@ -21,21 +24,56 @@ app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
 
-//build out changing status variable
-let status = '';
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function sendTweet() {
-  T.post('statuses/update', {status: 'thing3'}, (error, tweet, respsonse) => {
-    if (error) console.log(error);
+  client.get('https://api.chucknorris.io/jokes/random', (data, status) => {
+    var sentence = data.value.split(' ');
+    let index = getRandomIntInclusive(0, sentence.length);
+    let twitterStatus = `you are a treasonous ${sentence[index]}`;
 
-    console.log(tweet);
+    // T.post('statuses/update', {status: twitterStatus}, (error, tweet, respsonse) => {
+    //   if (error) console.log(error);
+    //   console.log('tweeted :)', tweet);
+    // });
   });
 }
 
-setInterval(() => {
-  try {
-    sendTweet();
-  } catch(e) {
-    console.log(e);
-  }
-}, 120000);
+
+function tweet() {
+  client.get('https://api.chucknorris.io/jokes/random', (data, status) => {
+    var sentence = data.value.split(' ');
+    let index = getRandomIntInclusive(0, sentence.length);
+    let twitterStatus = `you are a treasonous ${sentence[index]}`;
+
+    var image_path = './image.png';
+    var b64content = fs.readFileSync(image_path, { encoding: 'base64' });
+
+    T.post('media/upload', { media_data: b64content }, (err, data, response) => {
+      if (err) console.log(err);
+
+      console.log(data);
+
+      T.post('statuses/update', {status: twitterStatus, media_ids: data.media_id_string}, (error, tweet, respsonse) => {
+        if (error) console.log(error);
+
+        console.log('tweeted :)', tweet);
+      });
+    });
+  });
+}
+
+
+tweet();
+
+// setInterval(() => {
+//   try {
+//     sendTweet();
+//   } catch(e) {
+//     console.log(e);
+//   }
+// }, 120000);
